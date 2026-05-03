d0871e6f6a72ecb4877c083012efbb5ab86413c5
Ashleylq <ironmanashley@gmail.com>
2026-05-03 16:07:02 +0000

    Create navbar

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import { NavLink } from "react-router";
+import styles from "./Navbar.module.css"
+import { useContext } from "react";
+import userContext from "../../userContext.jsx"
 
 function Navbar(){
+    const {accessToken} = useContext(userContext);
     return (
-        <nav>
-            <NavLink to="/signup">Signup</NavLink>
-            <NavLink to="/login">Login</NavLink>
-        </nav>
+        <div className={styles.container}>
+            <h1 className={styles.title}>StairsAndAura</h1>
+            <nav className={styles.nav}>
+                <NavLink className={styles.link} to="/posts">Posts</NavLink>
+                <NavLink className={styles.link} to="/about">About Me</NavLink>
+                {!accessToken && <>
+                    <NavLink className={[styles.link, styles.bLink].join(' ')} to="/signup">Signup</NavLink>
+                    <NavLink className={[styles.link, styles.bLink].join(' ')} to="/login">Login</NavLink>
+                </>}
+            </nav>
+        </div>
     )
 }
 

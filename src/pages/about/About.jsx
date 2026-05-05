ba0d9d8ffaa36a271f00f06ed66b29826eff9f5d
Ashleylq <ironmanashley@gmail.com>
2026-05-05 06:45:05 +0000

    Create about page

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,12 @@
+import styles from "./About.module.css";
+
+function About(){
+    return (
+        <div className={styles.container}>
+            <h1>About me</h1>
+            <p>I climb stairs. I am the aura monster. The king of larp</p>
+        </div>
+    )
+}
+
+export default About;
